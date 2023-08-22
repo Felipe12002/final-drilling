704264f3196204cedcf33920248e47c608b56dca
Felipe Arias <correolukas1604@gmail.com>
2023-08-22 16:39:28 +0000

    incluyendo crud

--- laboratorio/models.py
+++ laboratorio/models.py
@@ -4,10 +4,21 @@ from django.db import models
 
 class Laboratorio(models.Model):
     nombre = models.CharField(max_length = 128)
+    cuidad = models.CharField(max_length = 128, default = "")
+    pais = models.CharField(max_length = 128, default = "")
+
+    def __str__(self):
+        return self.nombre
 
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length = 128)
     laboratorio = models.OneToOneField(Laboratorio, on_delete = models.PROTECT)
+    especialidad = models.CharField(max_length = 128, default = "")
+
+
+    def __str__(self):
+        return self.nombre
+
 
 class Producto(models.Model):
     nombre = models.CharField(max_length = 128)
@@ -15,3 +26,6 @@ class Producto(models.Model):
     f_fabricacion = models.DateField()
     p_costo = models.DecimalField(max_digits = 12, decimal_places = 2)
     p_venta = models.DecimalField(max_digits = 12, decimal_places = 2)
+
+    def __str__(self):
+        return self.nombre
