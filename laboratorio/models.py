1eb1d9fc33ce457c23ba69fde4476c55edc2818d
Felipe Arias <correolukas1604@gmail.com>
2023-08-22 00:28:44 +0000

    Implementacion de admin.py

--- laboratorio/models.py
+++ laboratorio/models.py
@@ -14,4 +14,4 @@ class Producto(models.Model):
     laboratorio = models.ForeignKey(Laboratorio, on_delete = models.PROTECT)
     f_fabricacion = models.DateField()
     p_costo = models.DecimalField(max_digits = 12, decimal_places = 2)
-    p_ventea = models.DecimalField(max_digits = 12, decimal_places = 2)
+    p_venta = models.DecimalField(max_digits = 12, decimal_places = 2)
